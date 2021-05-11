214064ff00bd6fb397e36dd1c8ca0ff9d872f760
shun-moku <prmnmbr13.blue828@gmail.com>
2021-05-11 21:11:52 +0900

    validation update

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,4 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
-
-  validates :nickname, presence: true, length: { minimum: 8 }
 end
