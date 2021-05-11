0f917e5da484341bc45b609c0a39a9e72637795f
shun-moku <prmnmbr13.blue828@gmail.com>
2021-05-11 23:51:13 +0900

    name→title

--- db/migrate/20210510225548_create_articles.rb
+++ db/migrate/20210510225548_create_articles.rb
@@ -1,7 +1,7 @@
 class CreateArticles < ActiveRecord::Migration[6.0]
   def change
     create_table :articles do |t|
-      t.string :name
+      t.string :title
       t.string :text
       t.text :image
       t.timestamps
