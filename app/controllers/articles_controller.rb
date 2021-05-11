46faf49f13a4814ce731d462447406fa814d2bba
shun-moku <prmnmbr13.blue828@gmail.com>
2021-05-11 10:03:31 +0900

    新規投稿とストロングパラメーターの設定

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,17 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+  def new
+    @article = Article.new
+  end
+
+  def create
+    Article.create(article_params)
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:name, :image, :text)
+  end
+
 end
