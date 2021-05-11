4ff6bf47b8e99a07a6741e17723931af6bcbe126
shun-moku <prmnmbr13.blue828@gmail.com>
2021-05-12 08:51:17 +0900

    コメントテーブルのアソシエーション

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,5 @@ class Article < ApplicationRecord
 
   validates :text, presence: true
   belongs_to :user
+  has_many :comments  # commentsテーブルとのアソシエーション
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,6 @@
 class Comment < ApplicationRecord
+
+  belongs_to :article  # tweetsテーブルとのアソシエーション
+  belongs_to :user  # usersテーブルとのアソシエーション
+
 end
