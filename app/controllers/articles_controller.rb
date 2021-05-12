bdbe1537568d15ca4853c02a82898b43fee84ffb
shun-moku <prmnmbr13.blue828@gmail.com>
2021-05-12 09:32:41 +0900

    コメント投稿機能

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -28,6 +28,8 @@ class ArticlesController < ApplicationController
   
   def show
     @article = Article.find(params[:id])
+    @comment = Comment.new
+    @comments = @article.comments.includes(:user)
   end
 
   private
