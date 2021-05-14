39cfe788a51ec290876d142c3e3af31f72ca966d
shun-moku <prmnmbr13.blue828@gmail.com>
2021-05-14 13:06:43 +0900

    Update 20210510225548_create_articles.rb

--- db/migrate/20210510225548_create_articles.rb
+++ db/migrate/20210510225548_create_articles.rb
@@ -2,7 +2,7 @@ class CreateArticles < ActiveRecord::Migration[6.0]
   def change
     create_table :articles do |t|
       t.string :title
-      t.string :text
+      t.longtext :text
 
       t.timestamps
     end
